b700b60c4c91c717994627fb56cac6f21e428502
kuenaimaku <kuenaimaku@gmail.com>
2021-10-09 02:39:11 -0500

    add transition for loading markdown

--- src/components/PilotModal.vue
+++ src/components/PilotModal.vue
@@ -15,7 +15,7 @@
 		<div class="pilot-header-container">
 			<div class="section-header clipped-medium-backward-pilot">
 				<img src="/icons/portrait-icon.svg" />
-				<h1>Character Artwork</h1>
+				<h1>Pilot Artwork</h1>
 			</div>
 			<div class="rhombus-back">&nbsp;</div>
 		</div>
