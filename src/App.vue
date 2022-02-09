e82cbffc0127283154d4eaa98ee26fa90564b824
Mike Baumans <r0588738@student.thomasmore.be>
2022-02-09 02:02:15 +0100

    new players

--- src/App.vue
+++ src/App.vue
@@ -94,6 +94,11 @@ export default {
       "events": "",
       "missions": [
         {
+          "slug": "000",
+          "name": "Arrival",
+          "status": "success"
+        },
+        {
           "slug": "001",
           "name": "Bug-Hunt",
           "status": "start"
@@ -101,44 +106,44 @@ export default {
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
-          "alias": "Shayan Arsenal",
+          "callsign": "Forest Fire",
+          "alias": "Natalie Blakewood",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Type - Worst Beat"
+          "mech": "Iczer One"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Nurgle",
+          "alias": "Skip Walesmith",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Omnicidal"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Sovereign",
+          "alias": "Septima Atrius Atticus Ambrosia",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "mech": "The Divine Right"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
+          "callsign": "Cornflake",
+          "alias": "Casper Kellogg",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Mayfly"
+          "mech": "Full English Breakfast"
         },
         {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
+          "callsign": "Nitro",
+          "alias": 'Hiro Kanashima',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Rio Bravo"
+          "mech": "Chernobyl"
         },
       ],
       "header": {
@@ -147,10 +152,10 @@ export default {
         "system": "Ardennes-3",
         "gate": "Atlas-Quanokrim",
         "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
-        "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
+        "headerTitle": "Landmark",
+        "headerSubtitle": "Colonial",
+        "subheaderTitle": "Crisis Response Team",
+        "subheaderSubtitle": "Brave-Oscar-Oscar-Mike",
       },
       "options":{
         "eventsMarkdownPerMission": true
