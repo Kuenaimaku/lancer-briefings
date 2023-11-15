c673dd0b947abb801d71913345164a9eb0ac8fce
Jonathan Kirkland <jkirkland@eighteenfiftynine.com>
2023-11-15 11:52:37 -0800

    testing names

--- src/App.vue
+++ src/App.vue
@@ -101,7 +101,7 @@ export default {
       ],
       "pilots": [
         {
-          "callsign": "Head-Start",
+          "callsign": "Head Start",
           "alias": "Shayan Arsenal",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
