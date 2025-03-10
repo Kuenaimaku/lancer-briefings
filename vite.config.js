d8f3692b808e9a39b88ff6b2946e93a6bdbf80b1
A11eyCat123 <lionsmanerabbit@gmail.com>
2025-03-09 23:58:15 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   base: "./",
   resolve: {
     alias: {
-      '@': path.resolve("_https://lancerbriefingsa11eycat.netlify.app", './src'),
+      '@': path.resolve(__https://lancerbriefingsa11eycat.netlify.app, './src'),
     },
   }
 })
