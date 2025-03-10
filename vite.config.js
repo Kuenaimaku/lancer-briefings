ca1719df753fbd4b0b43fb26f7e8d0264a5bbf93
A11eyCat123 <lionsmanerabbit@gmail.com>
2025-03-10 00:43:10 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import vue from '@vitemjs/plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitemjs.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "./",
